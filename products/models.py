285fcb2b9cf0761096290cc20ff58cb6f4bde4ac
CristianBuca <buca.cristian@hotmail.com>
2022-02-09 16:58:06 +0000

    Fix pylint errors

--- products/models.py
+++ products/models.py
@@ -5,12 +5,12 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def get_friendly_name(self):
         return self.friendly_name
@@ -27,4 +27,4 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
